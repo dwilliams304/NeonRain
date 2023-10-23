3629f55fbc054f634706f6b9b46f3d2b46708564
dwilliams304 <d.williams0434@gmail.com>
2023-10-23 11:30:44 -0700

    wip

--- Assets/_Scripts/Combat/Combat.cs
+++ Assets/_Scripts/Combat/Combat.cs
@@ -30,6 +30,7 @@ public class Combat : MonoBehaviour
     private float rangedMinDmg;
     private float rangedMaxDmg;
     private int rangedWeaponRange;
+    bool isRifle;
     private float rangedCritChance;
 
 #endregion
@@ -45,6 +46,8 @@ public class Combat : MonoBehaviour
 
     private bool isReloading;
     public bool didCrit = false;
+
+    private WaitForSeconds reloadSpeedWait;
 
     [Header("Melee Specific")]
     [SerializeField] Animation meleeSwing;
@@ -62,10 +65,16 @@ public class Combat : MonoBehaviour
 
 
     void Update(){
-        if(Input.GetButton("Fire1") && !isReloading){
+        /* Only add if I want it to where you can only hold down fire button with rifles
+        But otherwise have to shoot every time if its anything but an auto.
+        if(!isReloading && isRifle ? Input.GetButton("Fire1") : Input.GetButtonDown("Fire1")){
             Shoot();
         }
-        if(Input.GetButtonDown("Fire2")){
+        */
+        if(!isReloading && Input.GetButton("Fire1")){
+            Shoot();
+        }
+        else if(Input.GetButtonDown("Fire2")){
             Melee();
         }
         else if(Input.GetButtonDown("Reload")){
@@ -79,12 +88,15 @@ public class Combat : MonoBehaviour
     public void AssignRangedStats(Gun gun){
         fireRate = gun.fireRate;
         reloadSpeed = gun.reloadSpeed;
+        reloadSpeedWait = new WaitForSeconds(reloadSpeed); //cache this anytime we swap weapons and reload speed changes
         magSize = gun.magSize;
         currentAmmo = magSize;
         projectileSpeed = gun.projectileSpeed;
         rangedMinDmg = gun.minDamage;
         rangedMaxDmg = gun.maxDamage;
         rangedCritChance = gun.critChance;
+        if(gun.gunType == GunType.AutomaticRifle){ isRifle = true; }
+        else{ isRifle = false; }
         _uiMngr.UpdateAmmo(currentAmmo, magSize);
     }
 
@@ -101,16 +113,16 @@ public class Combat : MonoBehaviour
 
     void Shoot(){
         if(currentAmmo > 0){ //Do we have ammo?
-            if(Time.time > lastShot + (fireRate * FireRateMod)){ //If the last time we shot was more than the fire rate, we can shoot.
-                muzzleFlash.Play();
+            if(Time.time > lastShot + (fireRate * FireRateMod)){ //If the current time is greater than the last time we shot + current weapon's fire rate, we can shoot.
+                muzzleFlash.Play(); //Play particle effect!
                 lastShot = Time.time; //Start timer for the last time we shot
                 GameObject bullet = ObjectPooler.current.GetPooledPlayerBullet(); //Grab a bullet from the bullet pool
                 if(bullet == null) return; //If we don't have any bullets, do nothing (SHOULDNT HAPPEN)
                 bullet.transform.position = _firePoint.position;
-                bullet.transform.rotation = _firePoint.rotation; //Set the bullet to instantiate where the firing point is
+                bullet.transform.rotation = _firePoint.rotation; //Set the bullet to the firepoint's position and rotation
                 bullet.SetActive(true); //Set it active
                 bullet.GetComponent<Rigidbody2D>().AddForce(_firePoint.up * projectileSpeed, ForceMode2D.Impulse); //Add force to it
-                bullet.GetComponent<Bullet>().DamageAmount = CalculateDamage(true);
+                bullet.GetComponent<Bullet>().DamageAmount = CalculateDamage(true); //Set the calculated damage to the bullet's value.
                 currentAmmo--; //Remove ammo
                 _uiMngr.UpdateAmmo(currentAmmo, magSize); //Change the ammo text
             }
@@ -132,7 +144,7 @@ public class Combat : MonoBehaviour
     IEnumerator Reload(){
         isReloading = true; //Can't shoot while reloading, prevent that!
         _uiMngr.ReloadBar(reloadSpeed);
-        yield return new WaitForSeconds(reloadSpeed); //Wait as long as the weapon's reload speed is
+        yield return reloadSpeedWait; //Wait as long as the weapon's reload speed is
         currentAmmo = magSize; //Set our ammo to be equal to the max magazine size for the weapon
         isReloading = false; //They can reload again!
         _uiMngr.UpdateAmmo(currentAmmo, magSize); //Update UI
@@ -141,12 +153,12 @@ public class Combat : MonoBehaviour
     //Calculate and return the damage float, taking in a bool on whether it is a ranged attack or not
     //Needs bool to decided whether to take in melee weapon stats or ranged weapon stats
     //Better than having two separate methods that would do exactly this!
-    int CalculateDamage(bool isRanged){
+    int CalculateDamage(bool isRangedAttack){
         int critRoll = Random.Range(0, 100);
-        float dmgRoll = isRanged ? Random.Range(rangedMinDmg, rangedMaxDmg) : Random.Range(meleeMinDmg, meleeMaxDmg); //Check which weapon's damage stats to use
+        float dmgRoll = isRangedAttack ? Random.Range(rangedMinDmg, rangedMaxDmg) : Random.Range(meleeMinDmg, meleeMaxDmg); //Check which weapon's damage stats to use
         dmgRoll *= PlayerStatModifier.MOD_DamageDone; //No matter what this will apply.
 
-        if(critRoll <= PlayerStatModifier.MOD_CritChance + (isRanged ? rangedCritChance : meleeCritChance)){ //Check which weapon's crit chance to add onto this
+        if(critRoll <= PlayerStatModifier.MOD_CritChance + (isRangedAttack ? rangedCritChance : meleeCritChance)){ //Check which weapon's crit chance to add onto this
             didCrit = true;
             return Mathf.RoundToInt(dmgRoll *= PlayerStatModifier.MOD_CritDamage);
         }
