e3424ffbeab4d778e6dc01a61539bd9289b7f3cd
dwilliams304 <d.williams0434@gmail.com>
2023-11-02 11:51:01 -0700

    Add analytics

---
+++ Assets/_Scripts/Utility/GameStats.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStats : MonoBehaviour
+{
+    public static int damageDone;
+    public static int enemiesKilled;
+    public static int miniBossesKilled;
+    public static int bossesKilled;
+    public static int goldEarned;
+    public static int goldSpent;
+    public static int xpEarned;
+    public static int corruptionEarned;
+    public static int levelsGained;
+    public static int rerolls;
+    public static int chestsOpened;
+    public static int upgradesPicked;
+    public static int weaponsSwapped;
+}
