ac6207594256209c6751794a9fa15447915714c0
dwilliams304 <d.williams0434@gmail.com>
2023-11-06 13:11:38 -0700

    Remove DEV-Tools // rework

--- Assets/_Scripts/Utility/LootManager.cs
+++ Assets/_Scripts/Utility/LootManager.cs
@@ -21,9 +21,6 @@ public class LootManager : MonoBehaviour
     [SerializeField] GameObject _rarePrefab;
     [SerializeField] GameObject _corruptedPrefab;
     [SerializeField] GameObject _legendaryPrefab;
-    
-
-    public GameObject _dev_Panel;
 
 
     int _commonDropChance = 100000;
@@ -35,13 +32,11 @@ public class LootManager : MonoBehaviour
     
     public float AddedLuck = 0;
 
-    DEVTools _DEVTOOLS_;
 
 
     void Awake() => Instance = this;
 
     void Start(){
-        _DEVTOOLS_ = DEVTools.__DEV;
         SortIntoGroups();
     }
 
@@ -71,33 +66,27 @@ public class LootManager : MonoBehaviour
     }
 
     public void DropLoot(Vector3 position, float additionalLuck){
-        _DEVTOOLS_.totalRolls++;
         
         float rarity = Mathf.RoundToInt(Random.Range(0, 100001) / additionalLuck);
 
         if(rarity <= _commonDropChance && rarity > _uncommonDropChance) {
             SpawnLootObject(GenerateLootObject(_commonPrefab, _commonDrops.RandomFromList()), position);
-            _DEVTOOLS_.amntCommonDrops++; //DEV ONLY
         }
 
         else if(rarity <= _uncommonDropChance && rarity > _rareDropChance) {
             SpawnLootObject(GenerateLootObject(_uncommonPrefab, _uncommonDrops.RandomFromList()), position);
-            _DEVTOOLS_.amntUncommonDrops++; //DEV ONLY
         }
 
         else if(rarity <= _rareDropChance && rarity > _corruptedDropChance) {
             SpawnLootObject(GenerateLootObject(_rarePrefab, _rareDrops.RandomFromList()), position);
-            _DEVTOOLS_.amntRareDrops++; //DEV ONLY
         }
 
         else if(rarity <= _corruptedDropChance && rarity > _legendaryDropChance) {
             SpawnLootObject(GenerateLootObject(_corruptedPrefab, _corruptedDrops.RandomFromList()), position);
-            _DEVTOOLS_.amntCorruptDrops++; //DEV ONLY
         }
         
         else if(rarity <= _legendaryDropChance && rarity != _uniqueDropChance) {
             SpawnLootObject(GenerateLootObject(_legendaryPrefab, _legendaryDrops.RandomFromList()), position);
-            _DEVTOOLS_.amntLegendaryDrops++; //DEV ONLY
         }
     }
 
