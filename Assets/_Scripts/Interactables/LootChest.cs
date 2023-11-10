65661f77b4d05b21db89d816eaa0e407d2a0895d
dwilliams304 <d.williams0434@gmail.com>
2023-11-09 17:23:41 -0700

    Update Chest

--- Assets/_Scripts/Interactables/LootChest.cs
+++ Assets/_Scripts/Interactables/LootChest.cs
@@ -1,11 +1,39 @@
+using TMPro;
 using UnityEngine;
 
 public class LootChest : MonoBehaviour, IInteractable
 {
-
+    [SerializeField] private int _goldCost;
+    [SerializeField] private float _addtlLuck = 1f;
     [SerializeField] Vector3 lootDropOffset;
+
+    private TMP_Text infoText;
+
+    void Start(){
+        infoText = gameObject.GetComponentInChildren<TMP_Text>();
+        infoText.text = $"Press [E] to open chest. \n (Cost: {_goldCost} gold)";
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            infoText.enabled = true;
+            infoText.text = $"Press [E] to open chest. \n (Cost: {_goldCost} gold)";
+        }
+    }
+    void OnTriggerExit2D(Collider2D other){
+        if(other.tag == "Player"){
+            infoText.enabled = false;
+        }
+    }
+    
     public void Interacted(){
-        LootManager.Instance.DropLoot(gameObject.transform.position + lootDropOffset, 1f);
-        Destroy(gameObject);
+        if(Inventory.Instance.PlayerGold >= _goldCost){
+            Inventory.Instance.RemoveGold(_goldCost);
+            LootManager.Instance.DropLoot(gameObject.transform.position + lootDropOffset, _addtlLuck);
+            Destroy(gameObject);
+        }
+        else{
+            infoText.text = "Not enough gold!";
+        }
     }
 }
